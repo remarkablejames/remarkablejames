615687a1fa61c0b40fb8267ad936f561551ecd1b
James Niyongira <lascasasjames85@gmail.com>
2023-02-22 23:51:28 -0500

    initial setup of the navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f5f5f5",
+        secondary: "#2C3539",
+      },
+    },
+  },
+  plugins: [],
+};
